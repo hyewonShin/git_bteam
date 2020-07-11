2fe0d6c2fac869dbfbea2d393acf9d351f1b93f5
seesangwoo <washble@nate.com>
2020-07-11 16:23:08 +0900

    안지운거 지움

--- WithPet02/app/src/main/java/com/example/WithPet02/MainActivity.java
+++ WithPet02/app/src/main/java/com/example/WithPet02/MainActivity.java
@@ -552,34 +552,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private void checkDangerousPermissions() {
-        String[] permissions = {
-                Manifest.permission.INTERNET,
-                Manifest.permission.ACCESS_NETWORK_STATE,
-                Manifest.permission.ACCESS_WIFI_STATE
-        };
-
-        int permissionCheck = PackageManager.PERMISSION_GRANTED;
-        for (int i = 0; i < permissions.length; i++) {
-            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
-            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-                break;
-            }
-        }
-
-        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
-                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
-            } else {
-                ActivityCompat.requestPermissions(this, permissions, 1);
-            }
-        }
-    }
-
     //메인 광고판
     public void flipperImages(int image){
         ImageView imageView = new ImageView(this);
