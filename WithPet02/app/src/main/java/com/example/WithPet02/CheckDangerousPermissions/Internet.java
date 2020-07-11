bca5c0b946fbd0490958e9637027414b1d7683b4
seesangwoo <washble@nate.com>
2020-07-11 15:59:33 +0900

    위험권한 수정

---
+++ WithPet02/app/src/main/java/com/example/WithPet02/CheckDangerousPermissions/Internet.java
@@ -0,0 +1,50 @@
+package com.example.WithPet02.CheckDangerousPermissions;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class Internet {
+    Activity activity;
+    Context context;
+
+    //생성자 초기화
+    public Internet(Context context, Activity activity) {
+        this.context = context
+        this.activity = activity;
+    }
+
+    //위험권한
+    public void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+}
