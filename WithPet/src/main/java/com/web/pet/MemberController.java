9d97deb23013edb341a6ae7109f17294f2b33740
BBaek <qorentjdbds@naver.com>
2020-07-25 18:15:56 +0900

    대략적 틀

--- WithPet/src/main/java/com/web/pet/MemberController.java
+++ WithPet/src/main/java/com/web/pet/MemberController.java
@@ -1,5 +1,15 @@
 package com.web.pet;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
-	private int id;
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "";
+	}
+	
+	
 }
---
+++ WithPet/src/main/webapp/WEB-INF/views/include/left.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<div id="list" style="display: none;">
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>	
+	</ul>
+
+</div>
