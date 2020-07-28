c39a46ff9c9173feaeb6e02815295283ffb6b361
BBaek <qorentjdbds@naver.com>
2020-07-28 12:33:25 +0900

    qna

--- WithPet/src/main/java/com/web/pet/QnaController.java
+++ WithPet/src/main/java/com/web/pet/QnaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QnaController {
+	/* 사이트 QnA */
 	@RequestMapping("/list.qa")
 	public String list(HttpSession session) {
 		session.setAttribute("category", "qa");
@@ -14,6 +15,7 @@ public class QnaController {
 		return"qna/list";
 	}
 	
+	/* 전문가 QnA */
 	@RequestMapping("/faq.qa")
 	public String faq(HttpSession session) {
 		session.setAttribute("qanum", "faq");
